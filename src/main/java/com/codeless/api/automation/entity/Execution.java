bf1a7be5eacb60ffc2e2ee0cb7cba77438d67cf9
Oleg Strunevskiy <oleg.strunevskiy@gmail.com>
2020-11-26 12:19:53 -0800

    Extract entities related code to a separate project

---
+++ src/main/java/com/codeless/api/automation/entity/Execution.java
@@ -0,0 +1,47 @@
+package com.codeless.api.automation.entity;
+
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "executions")
+@Data
+public class Execution {
+
+  @Id
+  @GeneratedValue
+  private Long id;
+  @Column
+  private String name;
+  @Enumerated(EnumType.ORDINAL)
+  @Column
+  private ExecutionType type;
+  @Enumerated(EnumType.ORDINAL)
+  @Column
+  private ExecutionStatus status;
+  @Column(name = "execution_id")
+  private Long executionId;
+  @OneToOne
+  @JoinColumn(name = "region", referencedColumnName = "id")
+  private Region region;
+  @ManyToMany(targetEntity = Test.class)
+  @JoinTable(name = "tests_executions",
+      joinColumns = {@JoinColumn(name = "execution_id")},
+      inverseJoinColumns = {@JoinColumn(name = "test_id")})
+  private List<Test> tests;
+  @OneToOne(cascade = {CascadeType.ALL})
+  @JoinColumn(name = "result", referencedColumnName = "id")
+  private Result result;
+}
---
+++ src/main/java/com/codeless/api/automation/repository/ResultRepository.java
@@ -0,0 +1,8 @@
+package com.codeless.api.automation.repository;
+
+import com.codeless.api.automation.entity.Result;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResultRepository extends JpaRepository<Result, Long> {
+
+}
