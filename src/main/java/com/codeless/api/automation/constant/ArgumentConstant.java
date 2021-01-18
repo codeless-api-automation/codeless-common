625181620191608e0b04f8ede3ed6d2e08ae38fc
Oleg Strunevskiy <oleg.strunevskiy@gmail.com>
2021-01-17 21:36:17 -0800

    - Add one repository method to query execution by schedule id - Extract constants of tasks arguments

---
+++ src/main/java/com/codeless/api/automation/constant/ArgumentConstant.java
@@ -0,0 +1,9 @@
+package com.codeless.api.automation.constant;
+
+public interface ArgumentConstant {
+
+  String EXECUTION_TYPE = "executionType";
+  String SCHEDULE_ID = "scheduleId";
+  String EXECUTION_ID = "executionId";
+
+}
--- src/main/java/com/codeless/api/automation/repository/ExecutionRepository.java
+++ src/main/java/com/codeless/api/automation/repository/ExecutionRepository.java
@@ -1,10 +1,12 @@
 package com.codeless.api.automation.repository;
 
 import com.codeless.api.automation.entity.Execution;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ExecutionRepository extends JpaRepository<Execution, Long> {
 
+  Optional<Execution> findByScheduleId(Long scheduleId);
 }
