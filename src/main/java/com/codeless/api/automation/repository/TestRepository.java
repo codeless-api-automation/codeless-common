b5ca1a83ac4368498dc1bc83a53fc7f54f7f60bc
pavelklindziuk <pavelklindziuk@gmail.com>
2024-01-14 14:46:02 +0300

    CDLS: add username to repo and models

--- src/main/java/com/codeless/api/automation/repository/TestRepository.java
+++ src/main/java/com/codeless/api/automation/repository/TestRepository.java
@@ -11,7 +11,9 @@ import org.springframework.stereotype.Repository;
 public interface TestRepository extends JpaRepository<Test, Long> {
 
   Optional<Test> findByIdAndUsername(Long id, String username);
+
   void deleteByIdAndUsername(Long id, String username);
+
   Page<Test> findAllByUsername(String username, Pageable pageable);
 
 }
