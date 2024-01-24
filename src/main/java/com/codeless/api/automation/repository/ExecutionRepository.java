9c60b60d52cc2ea903402d39556a1830bcb2296d
pavelklindziuk <pavelklindziuk@gmail.com>
2024-01-24 17:07:33 +0300

    CDLS: add username to repo and models

--- src/main/java/com/codeless/api/automation/repository/ExecutionRepository.java
+++ src/main/java/com/codeless/api/automation/repository/ExecutionRepository.java
@@ -3,11 +3,16 @@ package com.codeless.api.automation.repository;
 import com.codeless.api.automation.entity.Execution;
 import com.codeless.api.automation.entity.ExecutionStatus;
 import java.util.List;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ExecutionRepository extends JpaRepository<Execution, Long> {
 
-  List<Execution> findAllByTestIdAndUsernameAndStatusIn(Long testId, String username, List<ExecutionStatus> statusList);
+  Optional<Execution> findByIdAndUsername(Long id, String username);
+  Page<Execution> findAllByUsername(String username, Pageable pageable);
+  List<Execution> findAllByTestIdAndUsernameAndStatusIn(Long id, String username, List<ExecutionStatus> statusList);
 }
--- src/main/java/com/codeless/api/automation/repository/ScheduleRepository.java
+++ src/main/java/com/codeless/api/automation/repository/ScheduleRepository.java
@@ -2,12 +2,15 @@ package com.codeless.api.automation.repository;
 
 import com.codeless.api.automation.entity.Schedule;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
+  Page<Schedule> findAllByUsername(String username, Pageable pageable);
   List<Schedule> findAllByTestIdAndUsername(Long testId, String username);
 
 }
